93a41b4282cb17218442c5cc08fad2215507ee8f
PavelChitaylo <snusmumrike@rambler.ru>
2020-11-02 19:18:44 +0300

    ActCompleted

---
+++ app/src/main/java/com/makentoshe/mathworks/ActCompletedActivity.kt
@@ -0,0 +1,23 @@
+package com.makentoshe.mathworks
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_act_completed.*
+
+class ActCompletedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_act_completed
+        )
+        var score=intent.getIntExtra("score",2)
+        var max=intent.getIntExtra("max",5)
+        var rate=(score.toDouble()/max.toDouble()*100.0).toInt()
+        scoreRateBar.progress=rate
+        scoreRateText.text= "$rate%"
+        backButton.setOnClickListener {
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
