b0dc885815eab624024f8a750bbd033ef901bef0
limoncatGod <Bliznec13>
2020-11-09 18:46:36 +0300

    New Files for Composite numbers

---
+++ app/src/main/java/com/makentoshe/mathworks/CompositeActivity.kt
@@ -0,0 +1,11 @@
+package com.makentoshe.mathworks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CompositeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.composite_1)
+    }
+}
