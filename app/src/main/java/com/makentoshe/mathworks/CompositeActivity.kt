5895c6956e23bd6735c20b3204c95353629d2ad9
limoncatGod <Bliznec13>
2020-11-16 17:19:27 +0300

    New tasks for composite and prime numbers

--- app/src/main/java/com/makentoshe/mathworks/CompositeActivity.kt
+++ app/src/main/java/com/makentoshe/mathworks/CompositeActivity.kt
@@ -1,11 +1,20 @@
 package com.makentoshe.mathworks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.multiple_1.*
 
 class CompositeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.composite_1)
+        ContinueMultiple.setOnClickListener {
+            val multIntent = Intent(this, CompositeActivity2::class.java)
+            multIntent.putExtra("progress", 1)
+            startActivity(multIntent)
+
+        }
+
     }
 }
