14fbca6f37fc3c3f51dab87a63e53abee882cb24
PavelChitaylo <snusmumrike@rambler.ru>
2021-02-22 23:58:34 +0300

    fix

--- app/src/main/java/com/makentoshe/mathworks/MenuSetup.kt
+++ app/src/main/java/com/makentoshe/mathworks/MenuSetup.kt
@@ -1,16 +1,47 @@
 package com.makentoshe.mathworks
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.layout_menu_setup.*
+
 
 class MenuSetup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_menu_setup)
         setupToMainButton.setOnClickListener {
-            startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "OVER9000", "My channel",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channel.description = "My channel description"
+            channel.enableLights(true)
+            channel.lightColor = Color.RED
+            channel.enableVibration(false)
+            notificationManager.createNotificationChannel(channel)
+        }
+        var builder = NotificationCompat.Builder(this, "OVER9000")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(getText(R.string.app_name))
+            .setContentText(getText(R.string.result_good))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(15, builder.build())
         }
     }
 }
