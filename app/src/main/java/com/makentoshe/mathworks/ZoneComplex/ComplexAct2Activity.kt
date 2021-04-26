2ff258e6d4a426d3b32e6db0f854c2ff6ae01217
PavelChitaylo <snusmumrike@rambler.ru>
2021-04-26 17:49:49 +0300

    commit

--- app/src/main/java/com/makentoshe/mathworks/ZoneComplex/ComplexAct2Activity.kt
+++ app/src/main/java/com/makentoshe/mathworks/ZoneComplex/ComplexAct2Activity.kt
@@ -90,8 +90,8 @@ class ComplexAct2Activity : AppCompatActivity() {
                 intt.putExtra("score", score)
                 intt.putExtra("max", max)
                 intt.putExtra("zone", "complex")
-                intt.putExtra("act", 0)
-                if (PreferenceManager.getDefaultSharedPreferences(applicationContext).getInt("statusComplexAct1", 0) < (score.toDouble() / max.toDouble() * 100.0).toInt()) PreferenceManager.getDefaultSharedPreferences(applicationContext).edit().putInt("statusComplexAct1", (score.toDouble() / max.toDouble() * 100.0).toInt()).apply()
+                intt.putExtra("act", 1)
+                if (PreferenceManager.getDefaultSharedPreferences(applicationContext).getInt("statusComplexAct2", 0) < (score.toDouble() / max.toDouble() * 100.0).toInt()) PreferenceManager.getDefaultSharedPreferences(applicationContext).edit().putInt("statusComplexAct2", (score.toDouble() / max.toDouble() * 100.0).toInt()).apply()
                 startActivity(intt)
             }
             radioGroupTask.clearCheck()
